5b5933211587583f3f36e9886fccaf44b60b0656
Chris <CGregoriou@uclan.ac.uk>
2018-11-23 23:32:28 +0200

    Created a test version of the leaderboard

--- web-app-api.js
+++ web-app-api.js
@@ -1,7 +1,34 @@
 const apiurl = "https://codecyprus.org/th/api/";
 
+function leaderboard() {
+    let xhttp = new XMLHttpRequest();
+    let leaderboardtable = document.getElementById("leaderboard");
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let object = JSON.parse(this.responseText);
+            for (let i = 0; i<object.leaderboard.length;i++) {
+                let tdplayer = document.createElement("td");
+                let tdscore = document.createElement("td");
+                tdplayer.innerHTML = object.leaderboard[i].player;
+                tdscore.innerHTML = object.leaderboard[i].score;
+                let newtr = document.createElement("tr");
+                newtr.appendChild(tdplayer);
+                newtr.appendChild(tdscore);
+                leaderboardtable.appendChild(newtr);
+            }
+        }
+        else {
+            //TODO If response not received (error).
+        }
+    };
+    let testurl= "https://codecyprus.org/th/test-api/leaderboard?sorted&size=10";
+    xhttp.open("GET",testurl, true);
+    xhttp.send();
+}
+
 function getTreasureHuntList() {
     let xhttp = new XMLHttpRequest();
+    var messages = document.getElementById("messages");
     let treasurehuntList = document.getElementById("treasurehuntlist");
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
@@ -25,6 +52,7 @@ function getTreasureHuntList() {
 }
 
 function displaylogin(e){
+    var uuid;
     uuid = e.target.attributes.id.value;
     document.getElementById("treasurehuntlistblock").style.display = "none";
     document.getElementById("login").style.display = "block";
@@ -49,35 +77,31 @@ function getquestion() {
 function displayQuestions() {
     document.getElementById("login").style.display = "none";
     //document.getElementById("...").style.display = "block";
-
 }
 
 function loginsubmit(){
     let v = document.getElementById("teamname").value;
-        messages.innerHTML = "";
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200){
-                let object = JSON.parse(this.responseText);
-                if (object.status === "OK"){
-                    messages.innerHTML = "";
-                    session = object.session;
-                    displayQuestions();
-                }
-                else if (object.status === "ERROR"){
-                    messages.innerHTML = object.errorMessages[0];
-                }
+    messages.innerHTML = "";
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200){
+            let object = JSON.parse(this.responseText);
+            if (object.status === "OK"){
+                messages.innerHTML = "";
+                var session;
+                session = object.session;
+                displayQuestions();
             }
-            else {
-                //TODO If response not received (error).
+            else if (object.status === "ERROR"){
+                messages.innerHTML = object.errorMessages[0];
             }
         }
-        let requesturl = apiurl + "start?player=" +v +"&app=Team1&treasure-hunt-id=" + uuid.toString();
-        xhttp.open("GET",requesturl,true);
-        xhttp.send();
+        else {
+            //TODO If response not received (error).
+        }
+    }
+    let requesturl = apiurl + "start?player=" +v +"&app=Team1&treasure-hunt-id=" + uuid.toString();
+    xhttp.open("GET",requesturl,true);
+    xhttp.send();
 }
-
-var session;
-var uuid;
-var messages = document.getElementById("messages");
 getTreasureHuntList();

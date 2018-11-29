c64b0fe693c5b99ba0b8e0af57ce70084e09a7c5
Chris <CGregoriou@uclan.ac.uk>
2018-11-29 09:40:16 +0200

    made the leaderboard empty if session is missing also i removed an the old inputbox. (havent be able to find the error with the question error yet)

--- web-app-api.js
+++ web-app-api.js
@@ -1,34 +1,54 @@
 const apiurl = "https://codecyprus.org/th/api/";
 
-function leaderboard() {
-    let xhttp = new XMLHttpRequest();
+function emptyLeaderboard() {
     let leaderboardtable = document.getElementById("leaderboard");
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            let object = JSON.parse(this.responseText);
-            for (let i = 0; i<object.leaderboard.length;i++) {
-                let tdranking = document.createElement("td");
-                let tdplayer = document.createElement("td");
-                let tdscore = document.createElement("td");
-                tdranking.innerHTML = i +1;
-                tdplayer.innerHTML = object.leaderboard[i].player;
-                tdscore.innerHTML = object.leaderboard[i].score;
-                let newtr = document.createElement("tr");
-                newtr.appendChild(tdranking);
-                newtr.appendChild(tdplayer);
-                newtr.appendChild(tdscore);
-                leaderboardtable.appendChild(newtr);
-            }
-        }
-        else {
-            //TODO If response not received (error).
-        }
-    };
-    let testurl= "https://codecyprus.org/th/test-api/leaderboard?sorted&size=10";
-    xhttp.open("GET",testurl, true);
-    xhttp.send();
+    for (let i = 0; i < 10; i++) {
+        let tdranking = document.createElement("td");
+        let tdplayer = document.createElement("td");
+        let tdscore = document.createElement("td");
+        tdranking.innerHTML = i + 1;
+        tdplayer.innerHTML = "-";
+        tdscore.innerHTML = "-";
+        let newtr = document.createElement("tr");
+        newtr.appendChild(tdranking);
+        newtr.appendChild(tdplayer);
+        newtr.appendChild(tdscore);
+        leaderboardtable.appendChild(newtr);
+    }
 }
+function leaderboard() {
+    if (session == "") {
 
+    }
+    else {
+        let xhttp = new XMLHttpRequest();
+        let leaderboardtable = document.getElementById("leaderboard");
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let object = JSON.parse(this.responseText);
+                for (let i = 0; i < object.leaderboard.length; i++) {
+                    let tdranking = document.createElement("td");
+                    let tdplayer = document.createElement("td");
+                    let tdscore = document.createElement("td");
+                    tdranking.innerHTML = i + 1;
+                    tdplayer.innerHTML = object.leaderboard[i].player;
+                    tdscore.innerHTML = object.leaderboard[i].score;
+                    let newtr = document.createElement("tr");
+                    newtr.appendChild(tdranking);
+                    newtr.appendChild(tdplayer);
+                    newtr.appendChild(tdscore);
+                    leaderboardtable.appendChild(newtr);
+                }
+            }
+            else {
+                //TODO If response not received (error).
+            }
+        };
+        let testurl = "https://codecyprus.org/th/test-api/leaderboard?sorted&size=10";
+        xhttp.open("GET", testurl, true);
+        xhttp.send();
+    }
+}
 function getTreasureHuntList() {
     let xhttp = new XMLHttpRequest();
     //var messages = document.getElementById("messages");
@@ -77,7 +97,7 @@ function getlocation() {
 function displaylogin(e){
     uuid = e.target.attributes.id.value;
     document.getElementById("treasurehuntlist").style.display = "none";
-    document.getElementById("welcome1").style.display = "none"
+    document.getElementById("welcome1").style.display = "none";
     document.getElementById("form2").style.display = "block";
     document.getElementById("welcome2").style.display = "block";
 }

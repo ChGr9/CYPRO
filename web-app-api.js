5dd681a44d43459d90824ea06756a35ef67cdc9e
Chris <CGregoriou@uclan.ac.uk>
2018-12-06 19:57:49 +0200

    Made the camera constantly on the rear camera

--- web-app-api.js
+++ web-app-api.js
@@ -1,41 +1,7 @@
 const apiurl = "https://codecyprus.org/th/api/";
 var camera = 0;
 
-var optsfront = {
-    // Whether to scan continuously for QR codes. If false, use scanner.scan() to
-    // manually scan. If true, the scanner emits the "scan" event when a QR code is
-    // scanned. Default true.
-    continuous: true,
-
-    // The HTML element to use for the camera's video preview. Must be a <video>
-    // element. When the camera is active, this element will have the "active" CSS
-    // class, otherwise, it will have the "inactive" class. By default, an invisible
-    // element will be created to host the video.
-    video: document.getElementById('preview'),
-
-    // Whether to horizontally mirror the video preview. This is helpful when trying to
-    // scan a QR code with a user-facing camera. Default true.
-    mirror: true,
-
-    // Whether to include the scanned image data as part of the scan result. See the
-    // "scan" event for image format details. Default false.
-    captureImage: false,
-
-    // Only applies to continuous mode. Whether to actively scan when the tab is not
-    // active.
-    // When false, this reduces CPU usage when the tab is not active. Default true.
-    backgroundScan: true,
-
-    // Only applies to continuous mode. The period, in milliseconds, before the same QR
-    // code will be recognized in succession. Default 5000 (5 seconds).
-    refractoryPeriod: 5000,
-
-    // Only applies to continuous mode. The period, in rendered frames, between scans. A
-    // lower scan period increases CPU usage but makes scan response faster.
-    // Default 1 (i.e. analyze every frame).
-    scanPeriod: 1
-};
-var optsrear = {
+var opts = {
     // Whether to scan continuously for QR codes. If false, use scanner.scan() to
     // manually scan. If true, the scanner emits the "scan" event when a QR code is
     // scanned. Default true.
@@ -69,8 +35,7 @@ var optsrear = {
     // Default 1 (i.e. analyze every frame).
     scanPeriod: 1
 };
-
-var scanner = new Instascan.Scanner(optsfront);
+var scanner = new Instascan.Scanner(opts);
 
 function QRreader() {
     document.getElementById("QRreader").style.display = "block";
@@ -78,10 +43,7 @@ function QRreader() {
     document.getElementById("stopqr").style.display = "block";
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-            scanner.start(cameras[camera]);
-            if (cameras.length > 1){
-                document.getElementById("switchcamera").style.display = "block";
-            }
+            scanner.start(cameras[1]);
         } else {
             console.error('No cameras found.');
             alert("No cameras found.");
@@ -100,19 +62,6 @@ function QRstop() {
     document.getElementById("QRreader").style.display = "none";
     document.getElementById("startqr").style.display = "block";
     document.getElementById("stopqr").style.display = "none";
-}
-
-function switchcamera() {
-    scanner.stop();
-    if (camera == 0){
-        camera = 1;
-        scanner = new Instascan.Scanner(optsrear);
-    }
-    else {
-        camera = 0;
-        scanner = new Instascan.Scanner(optsfront);
-    }
-    QRreader();
 }
 
 function emptyLeaderboard() {
